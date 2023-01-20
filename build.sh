d32fb0b94051adf5cd88c242dbb17f7305456de8
ricky yu <ricky@ifixit.com>
2023-01-20 12:52:00 -0700

    Fixed bug in build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf EncompassAPI
+rm -rf EncompassAPIphpSDK
 wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.20/swagger-codegen-cli-3.0.20.jar
 java -jar swagger-codegen-cli-3.0.20.jar generate -i \
  "https://app.swaggerhub.com/apiproxy/registry/Encompass.com/RestfulServices/1?resolved=true&flatten=true&pretty=true" \
- -l php -c EncompassAPIphpSDK/config.json \
+ -l php -c config.json \
  -o EncompassAPIphpSDK/
+rm -rf swagger-codegen-cli-3.0.20.jar
