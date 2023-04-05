83a3092bdae37627bf5766fcffe0cd86284ec13b
Daniel Beardsley <daniel@ifixit.com>
2023-04-04 17:06:55 -0700

    Code-gen: use newest version
    New version generates php 8.1 compatible files.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -rf EncompassAPIphpSDK
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.20/swagger-codegen-cli-3.0.20.jar
-java -jar swagger-codegen-cli-3.0.20.jar generate -i \
+wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.41/swagger-codegen-cli-3.0.41.jar
+java -jar swagger-codegen-cli-3.0.41.jar generate -i \
  "https://app.swaggerhub.com/apiproxy/registry/Encompass.com/RestfulServices/1?resolved=true&flatten=true&pretty=true" \
  -l php -c config.json \
- -o EncompassAPIphpSDK/
-rm -rf swagger-codegen-cli-3.0.20.jar
+ -o ./
+rm -rf swagger-codegen-cli-3.0.41.jar
