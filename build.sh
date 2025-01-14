2bf9b82ffec4267e933d4f660fdf4c0ee483605f
Michael Lahargou <michaell@ifixit.com>
2025-01-14 12:31:38 -0700

    codegen: update to 3.0.66
    This requires java 11.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CODEGEN_VERSION="3.0.66"
+
 rm -rf EncompassAPIphpSDK
-wget https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.41/swagger-codegen-cli-3.0.41.jar
-java -jar swagger-codegen-cli-3.0.41.jar generate -i \
+wget "https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/$CODEGEN_VERSION/swagger-codegen-cli-$CODEGEN_VERSION.jar"
+java -jar "swagger-codegen-cli-$CODEGEN_VERSION.jar" generate -i \
  "https://app.swaggerhub.com/apiproxy/registry/Encompass.com/RestfulServices/1?resolved=true&flatten=true&pretty=true" \
  -l php -c config.json \
  -o ./
-rm -rf swagger-codegen-cli-3.0.41.jar
+rm -rf "swagger-codegen-cli-$CODEGEN_VERSION.jar"
